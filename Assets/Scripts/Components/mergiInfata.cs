5bc0e86529b227dea39faa8098256dfc1792d8d4
WarPhX <adresadestuldeserioasa@gmail.com>
2019-06-29 16:42:52 +0300

    Level design
    + added the early attempt at a level design

---
+++ Assets/Scripts/Components/mergiInfata.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mergiInfata : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            GetComponent<Rigidbody>().velocity = transform.forward * 5;
+        }
+	}
+}
